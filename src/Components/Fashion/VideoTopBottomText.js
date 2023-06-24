974c3d015895c99a15c36e7948a0a92ec708becc
ankit723 <72332524+ankit723@users.noreply.github.com>
2023-06-24 18:22:23 +0530

    Fashion Page Added

--- src/Components/Fashion/VideoTopBottomText.js
+++ src/Components/Fashion/VideoTopBottomText.js
@@ -7,7 +7,7 @@ export default function VideoTopBottomText(props) {
     <div className="App">
       <div className="d-flex-flex-column"style={{maxWidth:"20rem"}}>
           <img src={props.imageurl} alt="New Blog Image" style={{width:`${props.imgSize}`, maxWidth:"100%", height:"auto"}}/>
-          <h4 style={{fontSize:`${props.headTextSize}`}}>{props.bottomText}</h4>
+          <h5 style={{fontSize:`${props.headTextSize}`}}>{props.bottomText}</h5>
           <h6>{`${props.author} - ${props.date}`}</h6>
           <p style={{fontSize:`${props.paraTextSize}`}}>{props.paragraph}</p>
       </div>
