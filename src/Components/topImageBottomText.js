50c975953935022d8ec44edcf48f8f612a8299f7
Abhinav Chaudhary <91717130+lan10rn@users.noreply.github.com>
2023-06-26 00:24:39 +0530

    Update topImageBottomText.js

--- src/Components/topImageBottomText.js
+++ src/Components/topImageBottomText.js
@@ -3,10 +3,10 @@ import React from "react";
 export function TopImageBottomText(props){
     return(
         <div className="App">
-            <div className="d-flex-flex-column vidTab"style={{maxWidth:"20rem"}}>
+            <div className="d-flex-flex-column vidTab m-2"style={{maxWidth:"20rem"}}>
                 <img src={props.topImg} alt="New Blog Image" style={{width:`${props.imgSize}`, maxWidth:"100%", height:"auto"}}/>
                 <h4 style={{fontSize:`${props.headTextSize}`}}>{props.bottomText}</h4>
-                <h6>{`${props.author} - ${props.date}`}</h6>
+                <h6 style={{fontSize:`${props.footTextSize}`}}>{`${props.date}`}</h6>
                 <p style={{fontSize:`${props.paraTextSize}`}}>{props.paragraph}</p>
             </div>
         </div>
