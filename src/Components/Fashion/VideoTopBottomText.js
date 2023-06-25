901bcaddf5039f41bcbe566dca58a4591ea033ea
Abhinav Chaudhary <91717130+lan10rn@users.noreply.github.com>
2023-06-26 00:18:39 +0530

    Update VideoTopBottomText.js

--- src/Components/Fashion/VideoTopBottomText.js
+++ src/Components/Fashion/VideoTopBottomText.js
@@ -5,10 +5,10 @@ export default function VideoTopBottomText(props) {
   return (
 
     <div className="App">
-      <div className="d-flex-flex-column"style={{maxWidth:"20rem"}}>
+      <div className="d-flex-flex-column vidTab"style={{maxWidth:"20rem"}}>
           <img src={props.imageurl} alt="New Blog Image" style={{width:`${props.imgSize}`, maxWidth:"100%", height:"auto"}}/>
           <h5 style={{fontSize:`${props.headTextSize}`}}>{props.bottomText}</h5>
-          <h6>{`${props.author} - ${props.date}`}</h6>
+          <h6 style={{fontSize:`${props.footTextSize}`}}>{`${props.author} - ${props.date}`}</h6>
           <p style={{fontSize:`${props.paraTextSize}`}}>{props.paragraph}</p>
       </div>
     </div>
