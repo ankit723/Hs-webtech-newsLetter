a4ab3b9726ee547310632114cc6bb06a4e5d14b9
Abhinav Chaudhary <91717130+lan10rn@users.noreply.github.com>
2023-06-26 00:19:36 +0530

    Update topVideoBottomText.js

--- src/Components/Videos/topVideoBottomText.js
+++ src/Components/Videos/topVideoBottomText.js
@@ -3,10 +3,10 @@ import React from "react";
 export function TopVideoBottomText(props){
     return(
         <div className="App">
-            <div className="d-flex-flex-column"style={{maxWidth:"20rem"}}>
+            <div className="d-flex-flex-column vidTab"style={{maxWidth:"20rem"}}>
                 <iframe src={props.videoUrl} sandbox="allow-same-origin allow-scripts" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" style={{width:`${props.videoSize}`, maxWidth:"100%", height:"auto"}}/>
                 <h5 style={{fontSize:`${props.headTextSize}`}}>{props.bottomText}</h5>
-                <h6>{`${props.author} - ${props.date}`}</h6>
+                <h6 style={{fontSize:`${props.footTextSize}`}}>{`${props.author} - ${props.date}`}</h6>
                 <p style={{fontSize:`${props.paraTextSize}`}}>{props.paragraph}</p>
             </div>
         </div>
