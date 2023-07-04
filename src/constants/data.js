ca707c99705761a3f403e0cf10f6a336938a48ce
Anuj Sharma <anujsharmawork11@gmail.com>
2023-07-04 16:04:24 +0530

    image paths updated

--- src/constants/data.js
+++ src/constants/data.js
@@ -19,19 +19,19 @@ const trendingNews = [
 const socialLinks = [
     {
         title: "Facebook",
-        logo: "/fb.webp",
+        logo: "/Hs-webtech-newsLetter/fb.webp",
         link: "https://www.facebook.com",
         alt: "Like"
     },
     {
         title: "Instagram",
-        logo: "/ig.png",
+        logo: "/Hs-webtech-newsLetter/ig.png",
         link: "https://www.instagram.com",
         alt: "Follow"
     },
     {
         title: "Youtube",
-        logo: "/yt.avif",
+        logo: "/Hs-webtech-newsLetter/yt.avif",
         link: "https://www.youtube.com",
         alt: "Subscribe"
     }
@@ -41,7 +41,7 @@ const priority = [
     {
         id: 1,
         category: "Business",
-        img: "/5.jpeg",
+        img: "/Hs-webtech-newsLetter/5.jpeg",
         title: "Successful Entrepreneurship: Strategies for Startup Growth",
         author: "John Smith",
         date: "2023-06-14"
@@ -49,7 +49,7 @@ const priority = [
     {
         id: 2,
         category: "Business",
-        img: "/6.jpeg",
+        img: "/Hs-webtech-newsLetter/6.jpeg",
         title: "Successful Entrepreneurship: Strategies for Startup Growth",
         author: "John Smith",
         date: "2023-06-14"
@@ -57,7 +57,7 @@ const priority = [
     {
         id: 3,
         category: "Business",
-        img: "/3.jpeg",
+        img: "/Hs-webtech-newsLetter/3.jpeg",
         title: "Successful Entrepreneurship: Strategies for Startup Growth",
         author: "John Smith",
         date: "2023-06-14"
@@ -65,7 +65,7 @@ const priority = [
     {
         id: 4,
         category: "Health & Fitness",
-        img: "/4.jpeg",
+        img: "/Hs-webtech-newsLetter/4.jpeg",
         title: "Boosting Immunity: Essential Vitamins and Nutrients for a Healthy Lifestyle",
         author: "Emily Johnson",
         date: "2023-06-14"
@@ -73,7 +73,7 @@ const priority = [
     {
         id: 5,
         category: "Recipes",
-        img: "/5.jpeg",
+        img: "/Hs-webtech-newsLetter/5.jpeg",
         title: "Delicious Summer Salads: Refreshing Recipes to Beat the Heat",
         author: "David Lee",
         date: "2023-06-13"
@@ -81,7 +81,7 @@ const priority = [
     {
         id: 6,
         category: "Recipes",
-        img: "/6.jpeg",
+        img: "/Hs-webtech-newsLetter/6.jpeg",
         title: "Delicious Summer Salads: Refreshing Recipes to Beat the Heat",
         author: "David Lee",
         date: "2023-06-13"
@@ -89,7 +89,7 @@ const priority = [
     {
         id: 6,
         category: "Travel",
-        img: "/7.jpeg",
+        img: "/Hs-webtech-newsLetter/7.jpeg",
         title: "Exploring Exotic Destinations: Unforgettable Adventures Around the World",
         author: "Sarah Thompson",
         date: "2023-06-12"
@@ -97,7 +97,7 @@ const priority = [
     {
         id: 6,
         category: "Health & Fitness",
-        img: "/8.jpeg",
+        img: "/Hs-webtech-newsLetter/8.jpeg",
         title: "Yoga for Stress Relief: Relaxing Poses for a Peaceful Mind",
         author: "Michael Johnson",
         date: "2023-06-11"
@@ -105,7 +105,7 @@ const priority = [
     {
         id: 6,
         category: "Business",
-        img: "/3.jpeg",
+        img: "/Hs-webtech-newsLetter/3.jpeg",
         title: "Digital Transformation: Adapting Businesses to the Digital Age",
         author: "Jennifer Adams",
         date: "2023-06-11"
@@ -113,7 +113,7 @@ const priority = [
     {
         id: 6,
         category: "Recipes",
-        img: "/7.jpeg",
+        img: "/Hs-webtech-newsLetter/7.jpeg",
         title: "Flavorful BBQ Recipes: Grilling Techniques for Perfectly Cooked Meats",
         author: "Robert Wilson",
         date: "2023-06-10"
@@ -121,7 +121,7 @@ const priority = [
     {
         id: 6,
         category: "Travel",
-        img: "/6.jpeg",
+        img: "/Hs-webtech-newsLetter/6.jpeg",
         title: "Hidden Gems: Unexplored Destinations Off the Beaten Path",
         author: "Sophia Turner",
         date: "2023-06-09"
@@ -129,7 +129,7 @@ const priority = [
     {
         id: 6,
         category: "Travel",
-        img: "/5.jpeg",
+        img: "/Hs-webtech-newsLetter/5.jpeg",
         title: "Hidden Gems: Unexplored Destinations Off the Beaten Path",
         author: "Sophia Turner",
         date: "2023-06-09"
@@ -137,7 +137,7 @@ const priority = [
     {
         id: 6,
         category: "Travel",
-        img: "/4.jpeg",
+        img: "/Hs-webtech-newsLetter/4.jpeg",
         title: "Hidden Gems: Unexplored Destinations Off the Beaten Path",
         author: "Sophia Turner",
         date: "2023-06-09"
@@ -145,7 +145,7 @@ const priority = [
     {
         id: 6,
         category: "Recipes",
-        img: "/3.jpeg",
+        img: "/Hs-webtech-newsLetter/3.jpeg",
         title: "Decadent Chocolate Desserts: Indulgent Treats for Chocolate Lovers",
         author: "Emma Carter",
         date: "2023-06-08"
@@ -153,7 +153,7 @@ const priority = [
     {
         id: 6,
         category: "Health & Fitness",
-        img: "/2.jpg",
+        img: "/Hs-webtech-newsLetter/2.jpg",
         title: "Morning Workout Routine: Energize Your Day with Exercise",
         author: "Daniel Martinez",
         date: "2023-06-07"
@@ -161,7 +161,7 @@ const priority = [
     {
         id: 6,
         category: "Health & Fitness",
-        img: "/1.jpg",
+        img: "/Hs-webtech-newsLetter/1.jpg",
         title: "Morning Workout Routine: Energize Your Day with Exercise",
         author: "Daniel Martinez",
         date: "2023-06-07"
@@ -173,7 +173,7 @@ const news = [
     {
         id: 1,
         category: "Fashion",
-        img: "/5.jpeg",
+        img: "/Hs-webtech-newsLetter/5.jpeg",
         title: "Successful Entrepreneurship: Strategies for Startup Growth",
         description: "We woke reasonably late following the feast and free flowing wine the night before. After gathering ourselves and our packs, we headed down to our homestay family’s small dining...",
         author: "John Smith",
@@ -182,7 +182,7 @@ const news = [
     {
         id: 2,
         category: "Travel",
-        img: "/6.jpeg",
+        img: "/Hs-webtech-newsLetter/6.jpeg",
         title: "Successful Entrepreneurship: Strategies for Startup Growth",
         description: "We woke reasonably late following the feast and free flowing wine the night before. After gathering ourselves and our packs, we headed down to our homestay family’s small dining...",
         author: "John Smith",
@@ -191,7 +191,7 @@ const news = [
     {
         id: 3,
         category: "Business",
-        img: "/3.jpeg",
+        img: "/Hs-webtech-newsLetter/3.jpeg",
         title: "Successful Entrepreneurship: Strategies for Startup Growth",
         description: "We woke reasonably late following the feast and free flowing wine the night before. After gathering ourselves and our packs, we headed down to our homestay family’s small dining...",
         author: "John Smith",
@@ -200,7 +200,7 @@ const news = [
     {
         id: 4,
         category: "Health & Fitness",
-        img: "/4.jpeg",
+        img: "/Hs-webtech-newsLetter/4.jpeg",
         title: "Boosting Immunity: Essential Vitamins and Nutrients for a Healthy Lifestyle",
         description: "We woke reasonably late following the feast and free flowing wine the night before. After gathering ourselves and our packs, we headed down to our homestay family’s small dining...",
         author: "Emily Johnson",
@@ -213,70 +213,70 @@ const someNews = [
         category: "Sports",
         author: "John Smith",
         title: "Record-Breaking Run: Athlete Shatters Marathon World Record",
-        img: "/1.jpg",
+        img: "/Hs-webtech-newsLetter/1.jpg",
         date: "2023-06-14"
     },
     {
         category: "Entertainment",
         author: "Emily Johnson",
         title: "Blockbuster Film 'The Legacy' Dominates Box Office",
-        img: "/2.jpg",
+        img: "/Hs-webtech-newsLetter/2.jpg",
         date: "2023-06-14"
     },
     {
         category: "Technology",
         author: "David Lee",
         title: "New AI-Powered Robot Assistant Takes Over Manufacturing Industry",
-        img: "/3.jpeg",
+        img: "/Hs-webtech-newsLetter/3.jpeg",
         date: "2023-06-13"
     },
     {
         category: "Business",
         author: "Sarah Thompson",
         title: "Company XYZ Reports Record Profits in Q2 Financial Results",
-        img: "/1.jpg",
+        img: "/Hs-webtech-newsLetter/1.jpg",
         date: "2023-06-12"
     },
     {
         category: "Science",
         author: "Michael Johnson",
         title: "Breakthrough Discovery: Scientists Uncover New Planet in Habitable Zone",
-        img: "/1.jpg",
+        img: "/Hs-webtech-newsLetter/1.jpg",
         date: "2023-06-11"
     },
     {
         category: "Health",
         author: "Jennifer Adams",
         title: "Groundbreaking Study Reveals Promising Treatment for Alzheimer's Disease",
-        img: "/1.jpg",
+        img: "/Hs-webtech-newsLetter/1.jpg",
         date: "2023-06-11"
     },
     {
         category: "Politics",
         author: "Robert Wilson",
         title: "Government Implements New Policy to Address Climate Change",
-        img: "/2.jpg",
+        img: "/Hs-webtech-newsLetter/2.jpg",
         date: "2023-06-10"
     },
     {
         category: "Travel",
         author: "Sophia Turner",
         title: "Top Travel Destinations for Summer 2023: A Guide for Wanderlust",
-        img: "/1.jpg",
+        img: "/Hs-webtech-newsLetter/1.jpg",
         date: "2023-06-09"
     },
     {
         category: "Fashion",
         author: "Emma Carter",
         title: "Fashion Week Extravaganza: Designers Showcase Latest Trends",
-        img: "/2.jpg",
+        img: "/Hs-webtech-newsLetter/2.jpg",
         date: "2023-06-08"
     },
     {
         category: "Food",
         author: "Daniel Martinez",
         title: "Culinary Delights: Discovering the Flavors of International Street Food",
-        img: "/2.jpg",
+        img: "/Hs-webtech-newsLetter/2.jpg",
         date: "2023-06-07"
     }
 ]
